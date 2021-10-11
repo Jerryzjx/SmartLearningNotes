97dc4db7a5d7e6f5a93ccb2200bdce07a0944788
JerryZhang0920 <jinxuan0920@gmail.com>
2021-10-12 03:05:58 +0800

    set DEBUG based on environment variables

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.http import Http404
@@ -15,8 +15,8 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic = Topic.objects.get(id=topic_id)
-
+    topic = get_object_or_404(Topic, id=topic_id)
+    
     #confirm the requested topic is owned by the current user
     if topic.owner != request.user:
         raise Http404
